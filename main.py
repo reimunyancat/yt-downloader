b267b0d156a7a852e4579db1ff283f0fad4e8ab3
reimunyancat <lasco647@gmail.com>
2025-03-31 16:42:32 +0900

    playlist download optimization

--- main.py
+++ main.py
@@ -1,18 +1,26 @@
 import yt_dlp
 import os
 
-def get_outtmpl(download_path, info_dict):
-    if 'playlist_title' in info_dict and info_dict['playlist_title']:
-        return os.path.join(download_path, '%(playlist_title)s/%(title)s.%(ext)s')
-    return os.path.join(download_path, '%(title)s.%(ext)s')
-
-def download_video(youtube_url, download_path):
+def get_info(youtube_url):
     with yt_dlp.YoutubeDL({'quiet': True}) as ydl:
         info = ydl.extract_info(youtube_url, download=False)
+    return info
+
+def download_video(youtube_url, download_path):
+    info = get_info(youtube_url)
+    
+    if 'entries' in info:
+        playlist_title = info.get('title', 'playlist')
+        playlist_path = os.path.join(download_path, playlist_title)
+        if not os.path.exists(playlist_path):
+            os.makedirs(playlist_path)
+        outtmpl = os.path.join(playlist_path, '%(title)s.%(ext)s')
+    else:
+        outtmpl = os.path.join(download_path, '%(title)s.%(ext)s')
     
     ydl_opts = {
         'format': 'bestvideo+bestaudio/best',
-        'outtmpl': get_outtmpl(download_path, info),
+        'outtmpl': outtmpl,
         'merge_output_format': 'mp4',
         'postprocessors': [{
             'key': 'FFmpegVideoConvertor',
@@ -33,12 +41,20 @@ def download_video(youtube_url, download_path):
         ydl.extract_info(youtube_url, download=True)
 
 def download_audio(youtube_url, download_path):
-    with yt_dlp.YoutubeDL({'quiet': True}) as ydl:
-        info = ydl.extract_info(youtube_url, download=False)
+    info = get_info(youtube_url)
+    
+    if 'entries' in info:
+        playlist_title = info.get('title', 'playlist')
+        playlist_path = os.path.join(download_path, playlist_title)
+        if not os.path.exists(playlist_path):
+            os.makedirs(playlist_path)
+        outtmpl = os.path.join(playlist_path, '%(title)s.%(ext)s')
+    else:
+        outtmpl = os.path.join(download_path, '%(title)s.%(ext)s')
     
     ydl_opts = {
         'format': 'bestaudio/best',
-        'outtmpl': get_outtmpl(download_path, info),
+        'outtmpl': outtmpl,
         'postprocessors': [{
             'key': 'FFmpegExtractAudio',
             'preferredcodec': 'mp3',
@@ -49,14 +65,20 @@ def download_audio(youtube_url, download_path):
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         ydl.extract_info(youtube_url, download=True)
 
-url = input('Enter the YouTube URL: ')
-fileformat = input('Enter the file format: video (1) or audio (2): ')
-download_path = 'downloads'
+def main():
+    url = input('Enter the YouTube URL: ')
+    fileformat = input('Enter the file format: video (1) or audio (2): ')
+    download_path = 'downloads'
 
-if not os.path.exists(download_path):
-    os.makedirs(download_path)
+    if not os.path.exists(download_path):
+        os.makedirs(download_path)
 
-if fileformat == '1':
-    download_video(url, download_path)
-elif fileformat == '2':
-    download_audio(url, download_path)
+    if fileformat == '1':
+        download_video(url, download_path)
+    elif fileformat == '2':
+        download_audio(url, download_path)
+    else:
+        print("Invalid option. Please choose 1 for video or 2 for audio.")
+
+if __name__ == '__main__':
+    main()
